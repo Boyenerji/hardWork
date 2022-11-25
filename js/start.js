66513fc2ec1f20e9360db1f864c78dee43cd70a8
bratik026 <boyenerji2014@yandex.ru>
2022-11-25 06:02:52 +0300

    upd

--- js/start.js
+++ js/start.js
@@ -37,9 +37,13 @@ let btn = document.querySelector('#btn_start'),
 
 
 
-    let a = ['\n', 'Животное', 'типа', 'ёжика\n']
-    a = a.filter(el => el.includes(/[a-я]/))
-    console.log(a)
+    // let a = ['\n', 'Животное', 'типа', 'ёжика\n'];
+    // a.forEach(function(item, i) {
+    //     console.log(item);
+    //     console.log(i);
+    // });
+    // a = a.filter(el => el.includes(/[a-я]/))
+    // console.log(a)
     
 
 // let bro = 'string';
